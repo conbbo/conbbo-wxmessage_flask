f81e2a61f53c84012ea98a7e62fa55c4ad21ebf1
kongbo <kongbo@staff.weibo.com>
2025-02-06 14:18:24 +0800

    add qr push

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,6 +4,8 @@ from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+import requests
+import json
 
 
 @app.route('/')
@@ -64,3 +66,46 @@ def get_count():
     """
     counter = Counters.query.filter(Counters.id == 1).first()
     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+
+
+@app.route('/api/getqr', methods=['GET'])
+def get_qr():
+    """
+    获取临时二维码
+    :return: 二维码ticket和url
+    """
+    try:
+        # 获取access_token (微信云托管环境下自动鉴权)
+        app_id = 'wx580dad6261cf35c6'
+        token_url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={app_id}'
+        token_response = requests.get(token_url)
+        access_token = token_response.json().get('access_token')
+
+        if not access_token:
+            return make_err_response('获取access_token失败')
+
+        # 生成临时二维码
+        qr_url = f'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token={access_token}'
+        qr_data = {
+            'expire_seconds': 2592000,  # 30天有效期
+            'action_name': 'QR_STR_SCENE',
+            'action_info': {
+                'scene': {
+                    'scene_str': 'test'
+                }
+            }
+        }
+        qr_response = requests.post(qr_url, json=qr_data)
+        qr_result = qr_response.json()
+
+        if 'ticket' not in qr_result:
+            return make_err_response('生成二维码失败')
+
+        # 返回二维码信息
+        return make_succ_response({
+            'ticket': qr_result['ticket'],
+            'url': qr_result['url'
+        })
+
+    except Exception as e:
+        return make_err_response(f'获取二维码失败: {str(e)}')
